4f59ee650cba2c8a23c2767b49e98a578b7f4a2b
alan <al6nlee@gmail.com>
2024-07-08 20:13:08 +0800

    init5

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 - grpc在如何在python上使用？
 - 完善框架问题
+
+
+## docker
+
+```bash
+docker run --rm -it  -p 7979:7878 al6nlee/grpcpy:py3_20240708_1e6b07d_v0.2
+```
--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,44 @@
 #!/bin/env bash
 
 cd ..
-docker build -t grpctmp:v0.7 -f docker/Dockerfile.grpc .
+
+build_image(){
+    if [[ $# != 3 ]]; then
+        echo "Usage : build_image image_name dockerfile context_path"
+        exit 0
+    fi
+    image_name=$1
+    dockerfile=$2
+    context_path=$3
+    docker build -t "${image_name}" -f "${dockerfile}" "${context_path}"
+    build_success=$?
+    if [[ ${build_success} -eq 0 ]]; then
+        echo "Build ### ${image_name} ### success!"
+    else
+        echo "Build ### ${image_name} ### failed!"
+        exit 1
+    fi
+}
+
+push_image(){
+    local_image=$1
+    if [[ $2 ]];then
+	    remote_image=$2
+	    docker tag "${local_image}" "${remote_image}"
+        docker push "${remote_image}"
+    else
+        docker push "${local_image}"
+    fi
+}
+
+CI_COMMIT_SHA=$(git log -1 --abbrev-commit --pretty=oneline | awk '{print $1}')
+commit_info_string=$(git log --pretty=format:"%H%cd" --date=format:'%Y%m%d' | grep ${CI_COMMIT_SHA})
+DATE=${commit_info_string:40:48}
+TAG=$1
+
+image=al6nlee/grpcpy:py3_${DATE}_${CI_COMMIT_SHA:0:7}_${TAG}
+# remote_image=""
+build_image "${image}" docker/Dockerfile.grpc .
+push_image "${image}"
+
+# sh build.sh v0.3
